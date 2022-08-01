6272a1a9e6a82015a87eea43eee356c0954acb73
RajaUmasree <102411766+RajaUmasree@users.noreply.github.com>
2022-08-01 10:26:24 +0530

    Updated

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -12,7 +12,7 @@
     TelCoColorCoder::ColorPair GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         TelCoColorCoder::MajorColor majorColor = 
-            (TelCoColorCoder::MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+            (TelCoColorCoder::MajorColor)(zeroBasedPairNumber / TelCoColorCoder::numberOfMinorColors);
         TelCoColorCoder::MinorColor minorColor =
             (TelCoColorCoder::MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
         return TelCoColorCoder::ColorPair(majorColor, minorColor);
