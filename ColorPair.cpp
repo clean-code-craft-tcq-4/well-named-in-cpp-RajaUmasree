21cdb05fb86b75ceb9049b2d4fc00797399a72a8
RajaUmasree <102411766+RajaUmasree@users.noreply.github.com>
2022-08-01 10:57:23 +0530

    Updated ColorPair.cpp

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 #include "ColorPair.h"
 
-    
-     std::string TelCoColorCoder::ColorPair::ToString() {
-         std::string colorPairStr = MajorColorNames[majorColor];
-         colorPairStr += " ";
-         colorPairStr += MinorColorNames[minorColor];
-         return colorPairStr;
-     }
 
-    TelCoColorCoder::ColorPair TelCoColorCoder::GetColorFromPairNumber(int pairNumber) {
+namespace TelCoColorCoder
+{
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"};
+    int numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    int numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+    GetColorFromPairNumber(int pairNumber);
+    GetColorFromPairNumber(int pairNumber);
+}
+
+std::string ColorPair::ToString() {
+      std::string colorPairStr = MajorColorNames[majorColor];
+      colorPairStr += " ";
+      colorPairStr += MinorColorNames[minorColor];
+      return colorPairStr;
+}
+
+ColorPair TelCoColorCoder::GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         TelCoColorCoder::MajorColor majorColor = 
             (TelCoColorCoder::MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
         TelCoColorCoder::MinorColor minorColor =
             (TelCoColorCoder::MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
         return TelCoColorCoder::ColorPair(majorColor, minorColor);
-    }
-    int TelCoColorCoder::GetPairNumberFromColor(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor) {
+}
+
+int TelCoColorCoder::GetPairNumberFromColor(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor) {
         return major * numberOfMinorColors + minor + 1;
-    }
+}
