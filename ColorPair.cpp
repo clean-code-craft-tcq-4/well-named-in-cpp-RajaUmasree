6e4295ca315688831aec8880fb29d16ecd56973f
RajaUmasree <102411766+RajaUmasree@users.noreply.github.com>
2022-08-01 10:44:25 +0530

    Updated

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -3,20 +3,20 @@
 
     
      std::string TelCoColorCoder::ColorPair::ToString() {
-         std::string colorPairStr = TelCoColorCoder::MajorColorNames[majorColor];
+         std::string colorPairStr = MajorColorNames[majorColor];
          colorPairStr += " ";
-         colorPairStr += TelCoColorCoder::MinorColorNames[minorColor];
+         colorPairStr += MinorColorNames[minorColor];
          return colorPairStr;
      }
 
     TelCoColorCoder::ColorPair TelCoColorCoder::GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         TelCoColorCoder::MajorColor majorColor = 
-            (TelCoColorCoder::MajorColor)(zeroBasedPairNumber / TelCoColorCoder::numberOfMinorColors);
+            (TelCoColorCoder::MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
         TelCoColorCoder::MinorColor minorColor =
-            (TelCoColorCoder::MinorColor)(zeroBasedPairNumber % TelCoColorCoder::numberOfMinorColors);
+            (TelCoColorCoder::MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
         return TelCoColorCoder::ColorPair(majorColor, minorColor);
     }
     int TelCoColorCoder::GetPairNumberFromColor(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor) {
-        return major * TelCoColorCoder::numberOfMinorColors + minor + 1;
+        return major * numberOfMinorColors + minor + 1;
     }
